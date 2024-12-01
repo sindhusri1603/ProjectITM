98b7de5b7126bca18aec217b08efc2f04e2770d3
sindhusri1603 <112238963+sindhusri1603@users.noreply.github.com>
2024-12-01 14:03:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post('/api/submit', async (req, res) => {
     const { name, email, className, location } = req.body;
     const newForm = new Form({ name, email, className, location });
 console.log(location)
-    if(location=="St. Albert"){
+    if(location.trim()=="St. Albert"){
         try {
             await newForm.save(); // Save to the database
             res.json({ message: 'Data successfully submitted!' });
