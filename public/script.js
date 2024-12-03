f12296f0c558d806d64aac5e162702a7e8381f22
sindhusri1603 <112238963+sindhusri1603@users.noreply.github.com>
2024-12-03 11:04:36 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,8 +1,7 @@
 
 
-const classroomCoords = { lat: 53.631876, lon: -113.589990 };  // Classroom coordinates
-        const allowedRadius = 35;  // Allowed radius in meters
-
+const classroomCoords = { lat: 53.560131, lon: -113.446030 };  // Classroom coordinates
+        const allowedRadius = 10;  // Allowed radius in meters
         function getLoc() {
             if (!navigator.geolocation) {
                 document.getElementById('locationDisplay').innerText = "Geolocation is not supported by your browser.";
@@ -22,7 +21,7 @@ const classroomCoords = { lat: 53.631876, lon: -113.589990 };  // Classroom coor
                         document.getElementById('message').innerText = "";
                     } else {
                         document.getElementById('locationDisplay').innerText = `Location: Outside allowed area (${Math.round(distance)} meters away)`;
-                        document.getElementById('message').innerText = "You must be within the classroom area (75 meters) to mark attendance.";
+                        document.getElementById('message').innerText = "You must be within the classroom area (10 meters) to mark attendance.";
                     }
                 },
                 (error) => {
